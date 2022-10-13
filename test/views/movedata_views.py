3ffbb55901c76fadb7b37c8f4a87fa8e9988e99c
Bo-Seong Lim <brudenell526@gmail.com>
2022-10-13 19:37:40 +0900

    sac test

--- test/views/movedata_views.py
+++ test/views/movedata_views.py
@@ -11,3 +11,15 @@ bp = Blueprint('movedata', __name__, url_prefix='/movedata')
 def _list():
     movedata_list = Movedata.query.order_by(Movedata.id)
     return render_template('movedata/movedata_list.html', movedata_list=movedata_list)
+
+
+@bp.route('sac')
+def sac():
+    name = request.form['name']
+    user = User.query.filter_by(name=name).first()
+    movedata = Movedata.query.filter_by(user_id=user.id).first()
+    movedata.latitude = float(request.form['latitude'])
+    movedata.longitude = float(request.form['longitude'])
+    movedata.speed = request.form['speed']
+    db.session.commit()
+    return jsonify(id=user.id, success=True)
